867a31b8cc09d3e08742e15e27ef3f4116d09560
naamaevenoz <naama.evenoz@mail.huji.ac.il>
2025-09-15 14:28:24 +0300

    Added WaveConfig script

---
+++ Assets/Scripts/Hidden Points System/WaveConfig.cs
@@ -0,0 +1,18 @@
+﻿using System.Drawing;
+
+namespace Hidden_Points_System
+{
+    [System.Serializable]
+    public class WaveConfig {
+        public float speed;
+        public int waveAmount;
+        public float noiseAmp;
+        public float noiseScale;
+        public Color color;
+
+        public int targetAmount;
+        public float spawnDelay;
+        public float lifetime;
+    }
+
+}
