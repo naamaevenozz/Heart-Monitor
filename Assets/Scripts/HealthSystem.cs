d3054899bf9094dead6d91fa5419e81b3a5d80aa
edenavrahami <eden.avrahami@mail.huji.ac.il>
2025-09-15 14:11:18 +0300

    Health and Score systems

---
+++ Assets/Scripts/HealthSystem.cs
@@ -0,0 +1,38 @@
+namespace DefaultNamespace
+{
+    using UnityEngine;
+    namespace Player
+    {
+        public class HealthSystem : MonoBehaviour
+        {
+            public int maxHealth = 1;
+            private int currentHealth;
+
+            private void Start()
+            {
+                currentHealth = maxHealth;
+                GameEvents.LivesChanged?.Invoke(currentHealth);
+            }
+
+            public void TakeDamage(int amount)
+            {
+                currentHealth -= amount;
+                GameEvents.LivesChanged?.Invoke(currentHealth);
+
+                if (currentHealth <= 0)
+                {
+                    GameEvents.GameOver?.Invoke();
+                }
+            }
+            
+            public void AddLives(int amount)
+            {
+                currentHealth += amount;
+                if (currentHealth > maxHealth)
+                    currentHealth = maxHealth;
+                GameEvents.LivesChanged?.Invoke(currentHealth);
+            }
+        }
+
+    }
+}
--- Assets/Scripts/Utils/GameEvents.cs
+++ Assets/Scripts/Utils/GameEvents.cs
@@ -6,8 +6,10 @@ namespace DefaultNamespace
     {
         public static Action RestartLevel;
         public static Action StartLevel;
-        public static Action PlayerLostLife;
         public static Action<int> ScoreChanged;     // int = new score
         public static Action<int> LivesChanged; 
+        public static Action GameOver;
+        public static Action GameStarted;
+        public static Action Intro;
     }
 }
