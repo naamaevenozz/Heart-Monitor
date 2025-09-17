9f7a77141154c58641f02fbf0b02099f804b7edd
edenavrahami <eden.avrahami@mail.huji.ac.il>
2025-09-17 14:13:52 +0300

    target count ui

---
+++ Assets/Scripts/UI/UITargetCount.cs
@@ -0,0 +1,14 @@
+using TMPro;
+using UnityEngine;
+
+namespace UI
+{
+    public class UITargetCount : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text targetCountText;
+        
+        
+        
+        
+    }
+}
